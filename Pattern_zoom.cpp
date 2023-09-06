4118acf58dc6ea5478b9de940fc20ea3573836d0
FirstGibon <102861099+Permyakov-Dmitriy@users.noreply.github.com>
2023-09-06 23:25:01 +0600

    Add files via upload

---
+++ Pattern_zoom.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+#include <algorithm>
+#include <windows.h>
+#include <deque>
+
+
+using namespace std;
+
+
+int main() {
+    SetConsoleCP(65001);
+    SetConsoleOutputCP(65001);
+
+    int num;
+    deque<string> rows;
+
+    cin >> num;
+
+    for (size_t j = 1; j < num / 2 + 2; j++)
+    {
+        string chs = "";
+
+        for (size_t i = 1; i < num / 2 + 2; i++)
+        {
+            if (i < j)
+            {
+                if (j % 2)
+                {
+                    chs += "■";
+                } 
+                else
+                {
+                    chs += "□";
+                }
+            }
+            else
+            {
+                if (i % 2)
+                {
+                    chs += "■";
+                } 
+                else
+                {
+                    chs += "□";
+                }
+            }
+        }
+
+        string mir = chs.substr(3, size(chs) - 3);
+
+        for (size_t i = 0; i < size(mir); i+=3)
+        {
+            chs = mir.substr(i, 3) + chs;
+        }
+
+        rows.push_back(chs);
+    }
+
+    deque<string>::iterator start = rows.begin() + 1;
+    deque<string>::iterator end = rows.end(); 
+
+    for (deque<string>::iterator it = start; it != end; ++it)
+    {
+        rows.push_front(*it);
+    }
+
+    for (auto &&i : rows)
+    {
+        cout << i << endl;
+    }
+
+    system("pause");
+
+    return 0;
+}
